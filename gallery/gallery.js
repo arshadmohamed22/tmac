74e326fcf86e99c3a92373c300431231edefdb57
arshadmohamed22 <38231213+arshadmohamed22@users.noreply.github.com>
2018-07-24 17:22:05 -0400

    Create gallery.js

---
+++ gallery/gallery.js
@@ -0,0 +1,74 @@
+$(document).ready(function(){
+	var event1 = $("#event1").offset().top - 100;
+	var event2 = $("#event2").offset().top - 100;
+	var event3 = $("#event3").offset().top - 100;
+	var event4 = $("#event4").offset().top - 100;
+	var event5 = $("#event5").offset().top - 100;
+	var event6 = $("#event6").offset().top - 100;
+	var event7 = $("#event7").offset().top - 100;
+	var event8 = $("#event8").offset().top - 100;
+	var event9 = $("#event9").offset().top - 100;
+	$("#e1").click(function() {
+		$('html, body').animate({
+			scrollTop: event1
+		}, 1000);
+	});
+	$("#e2").click(function() {
+		$('html, body').animate({
+			scrollTop: event2
+		}, 1000);
+	});
+	$("#e3").click(function() {
+		$('html, body').animate({
+			scrollTop: event3
+		}, 1000);
+	});
+	$("#e4").click(function() {
+		$('html, body').animate({
+			scrollTop: event4
+		}, 1000);
+	});
+	$("#e5").click(function() {
+		$('html, body').animate({
+			scrollTop: event5
+		}, 1000);
+	});
+	$("#e6").click(function() {
+		$('html, body').animate({
+			scrollTop: event6
+		}, 1000);
+	});
+	$("#e7").click(function() {
+		$('html, body').animate({
+			scrollTop: event7
+		}, 1000);
+	});
+	$("#e8").click(function() {
+		$('html, body').animate({
+			scrollTop: event8
+		}, 1000);
+	});
+	$("#e9").click(function() {
+		$('html, body').animate({
+			scrollTop: event9
+		}, 1000);
+	});
+
+	// When the user scrolls down 20px from the top of the document, show the button
+	window.onscroll = function() {scrollFunction()};
+
+	function scrollFunction() {
+	    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+	        document.getElementById("top").style.display = "block";
+	    } else {
+	        document.getElementById("top").style.display = "none";
+	    }
+	}
+
+	// When the user clicks on the button, scroll to the top of the document
+	$("#top").click(function() {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 1000);
+	});
+});
