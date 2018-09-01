b84727893c6e7dd2a94e4fa94f5b0b30d81e1318
arshadmohamed22 <38231213+arshadmohamed22@users.noreply.github.com>
2018-09-01 14:22:36 -0400

    Add files via upload

---
+++ about/about.js
@@ -0,0 +1,11 @@
+
+	var popup = document.getElementById('Popup');
+	//var btn1 = document.getElementById("o1");
+	var close1 = document.getElementsByClassName("close")[0];
+
+	function openPopup() {
+		popup.style.display = "block";
+	}
+	function closePopup() {
+		popup.style.display = "none";
+	}
